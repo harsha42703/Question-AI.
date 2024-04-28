766c11c830d5dd79537eef89f6ef106d2abc2164
Harsha Vardhan <94213293+harsha42703@users.noreply.github.com>
2024-04-28 17:36:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ function App() {
   );
 
   return (
-    <div className="min-h-screen overflow-x-hidden w-screen bg-blue-50">
+    <div className="h-screen overflow-x-hidden w-screen bg-blue-50">
       <header className="sticky top-0 bg-black shadow">
         <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center py-4 md:justify-start md:space-x-10">
